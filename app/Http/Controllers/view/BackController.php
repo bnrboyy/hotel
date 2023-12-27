e42e451c1ce9a70ac856fd984e807390c3b51920
bnrboyy <nantachai.ru@gmail.com>
2023-12-27 20:16:31 +0700

    update

--- app/Http/Controllers/view/BackController.php
+++ app/Http/Controllers/view/BackController.php
@@ -18,7 +18,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use PhpParser\Node\Expr\Cast\Bool_;
 
 class BackController extends Controller
 {
